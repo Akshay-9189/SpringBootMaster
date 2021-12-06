ea80fb09c48f6c07182e37ed272029a544f4f149
Akshay-9189 <akshaykumbhar957@gmail.com>
2021-12-06 20:28:55 +0530

    Services Added

--- SpringBootApplication/SpringBootApplication/src/main/java/com/csi/com/csi/controller/ApplicationController.java
+++ SpringBootApplication/SpringBootApplication/src/main/java/com/csi/com/csi/controller/ApplicationController.java
@@ -17,4 +17,9 @@ public class ApplicationController {
 
         return  "Welcome To CSI";
     }
+    
+    @GetMapping("/services")
+    public String csiServices(){
+    	return "Payment Gateway";
+    }
 }
