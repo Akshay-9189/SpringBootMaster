13e4d60ad664dcc97e6ac82bf0884d579881c2e1
Akshay-9189 <akshaykumbhar957@gmail.com>
2021-12-06 19:44:27 +0530

    Initial Programme

---
+++ SpringBootApplication/SpringBootApplication/src/main/java/com/csi/com/csi/controller/ApplicationController.java
@@ -0,0 +1,20 @@
+package com.csi.com.csi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by HP on 06-Dec-21.
+ */
+
+@RestController
+@RequestMapping("/")
+public class ApplicationController {
+
+    @GetMapping("/hi")
+    public String sayHello(){
+
+        return  "Welcome To CSI";
+    }
+}
